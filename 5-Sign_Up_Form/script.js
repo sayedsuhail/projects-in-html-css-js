effe12159fdcb182f952619d6fbb03a60dcd992f
Sayed Suhail <sayedsuhail006@gmail.com>
2023-09-10 22:15:32 +0500

    project - sign-up form added

---
+++ 5-Sign_Up_Form/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container")
+const btn_signUp = document.querySelector(".green-bg button")
+
+btn_signUp.addEventListener("click", () => {
+  container.classList.toggle("change")
+})
