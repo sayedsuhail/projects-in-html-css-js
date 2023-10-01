a6dfbd39b2d402f25a6155815864a293ebe26ac9
Sayed Suhail <sayedsuhail006@gmail.com>
2023-10-01 16:11:42 +0500

    project animated-navigation added

---
+++ 23-Animated_Navigation/script.js
@@ -0,0 +1,26 @@
+const menuToggle = document.querySelector("#menu-toggle")
+const activeElements = document.querySelectorAll(".active-element")
+
+menuToggle.addEventListener("click", () => {
+  for (let active = 0; active < activeElements.length; active++) {
+    activeElements[active].classList.toggle("active")
+  }
+
+  const navBtnIcon = menuToggle.childNodes[1]
+  navBtnIcon.classList.toggle("fa-times")
+
+  const pageBg = document.querySelector(".main-container")
+
+  if (navBtnIcon.classList.contains("fa-times")) {
+    pageBg.style.filter = "blur(3px)"
+  } else {
+    setTimeout(() => {
+      pageBg.style.filter = "blur(0)"
+    }, 500)
+  }
+
+  const listItems = document.querySelectorAll(".main-nav-list a")
+  listItems.forEach((item, index) => {
+    item.style.transition = `all .5s linear ${index * 0.3}s`
+  })
+})
