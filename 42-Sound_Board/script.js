e44e4a92ed5784d35f8602938cedbc9c51383222
Sayed Suhail <sayedsuhail006@gmail.com>
2023-10-29 10:22:57 +0500

    js done for play and pause sound

--- 42-Sound_Board/script.js
+++ 42-Sound_Board/script.js
@@ -6,5 +6,23 @@ sounds.forEach((sound) => {
 
   btn.innerText = sound
 
+  // play sound when button clicked
+  btn.addEventListener("click", () => {
+    // stop other sounds
+    stopSounds()
+
+    // play current sound
+    document.getElementById(sound).play()
+  })
+
   document.getElementById("buttons").appendChild(btn)
 })
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const audio = document.getElementById(sound)
+
+    audio.pause()
+    audio.currentTime = 0
+  })
+}
